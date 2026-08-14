93b8a9e64691adadad3cff5776177dadef49296c
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-08-14 10:34:40 -0300

    feat: add login function in LoginViewModel. Refactored function in CadastroArtesaoViewModel

--- app/src/main/java/com/example/nprojetoartesanato/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/nprojetoartesanato/ui/login/LoginViewModel.kt
@@ -4,13 +4,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.nprojetoartesanato.data.repository.ArtesaoRepository
+import com.example.nprojetoartesanato.data.session.SessionManager
 
 class LoginViewModel : ViewModel() {
+
+    //private val repository = ArtesaoRepository()
+
     var usuario by mutableStateOf("")
         private set
 
     var senha by mutableStateOf("")
         private set
+
+    var erro by mutableStateOf<String?>(null)
+        private set
+
+//    fun atualizarUsuario(valor: String) {
+//        usuario = valor
+//    }
+//
+//    fun atualizarSenha(valor: String) {
+//        senha = valor
+//    }
 
     fun onUsuarioChange(newValue: String) {
         usuario = newValue
@@ -19,4 +35,49 @@ class LoginViewModel : ViewModel() {
     fun onSenhaChange(newValue: String) {
         senha = newValue
     }
+
+    fun login(): Boolean {
+
+        if (usuario.isBlank() || senha.isBlank()) {
+
+            erro = "Usuário e senha são obrigatórios."
+
+            return false
+        }
+
+        val artesao = ArtesaoRepository.autenticar(
+            usuario = usuario,
+            senha = senha
+        )
+
+        if (artesao == null) {
+
+            erro = "Usuário ou senha inválidos."
+
+            return false
+        }
+
+        SessionManager.iniciarSessao(artesao)
+
+        erro = null
+
+        return true
+    }
 }
+
+/*
+
+Current situation:
+
+val artesao = ArtesaoRepository.autenticar(...) searches in memory but later we can use
+the same method to:
+
+repository.autenticar()
+       ↓
+Retrofit
+       ↓
+POST /auth/login
+       ↓
+Spring Boot
+
+ */
