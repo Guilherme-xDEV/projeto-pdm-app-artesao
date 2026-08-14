983c8715e85a963065e35b945d2f70660a36b6aa
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-08-14 08:02:32 -0300

    feat: repository class for Artesao object

--- app/src/main/java/com/example/nprojetoartesanato/data/repository/ArtesaoRepository.kt
+++ app/src/main/java/com/example/nprojetoartesanato/data/repository/ArtesaoRepository.kt
@@ -1,4 +1,38 @@
 package com.example.nprojetoartesanato.data.repository
 
+import com.example.nprojetoartesanato.data.local.LocalDataStore
+import com.example.nprojetoartesanato.model.Artesao
+
 class ArtesaoRepository {
+
+    fun cadastrar(artesao: Artesao): Artesao {
+
+        return LocalDataStore.adicionarArtesao(artesao)
+    }
+
+    fun autenticar(
+        usuario: String,
+        senha: String
+    ): Artesao? {
+
+        return LocalDataStore.buscarArtesao(
+            usuario = usuario,
+            senha = senha
+        )
+    }
 }
+
+/*
+
+Current:
+ArtesaoRepository
+      ↓
+LocalDataStore
+
+Later:
+ArtesaoRepository
+      ↓
+Retrofit
+      ↓
+Spring Boot
+ */
