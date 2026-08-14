e66c6b0ae2483322679fd0f37bc63c4e9998a42f
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-08-14 18:09:04 -0300

    feat: implemented Repository and ViewModel for Produto entity

--- app/src/main/java/com/example/nprojetoartesanato/data/repository/ProdutoRepository.kt
+++ app/src/main/java/com/example/nprojetoartesanato/data/repository/ProdutoRepository.kt
@@ -1,4 +1,23 @@
 package com.example.nprojetoartesanato.data.repository
 
+import com.example.nprojetoartesanato.data.local.LocalDataStore
+import com.example.nprojetoartesanato.model.Produto
+
 class ProdutoRepository {
+
+    fun cadastrar(
+        produto: Produto
+    ): Produto {
+
+        return LocalDataStore.adicionarProduto(produto)
+    }
+
+    fun buscarProduto(
+        produto: Produto
+    ): List<Produto> {
+
+        return LocalDataStore.buscarProdutosDoArtesao(
+            produto.artesaoId // verify if this works!
+        )
+    }
 }
