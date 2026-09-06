31407db0f176c5afe8d1854abd9da244d2ee2a27
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-09-06 18:21:48 -0300

    refactor: change local url to railway deploy url

--- app/src/main/java/com/example/nprojetoartesanato/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/nprojetoartesanato/data/network/RetrofitClient.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://192.168.0.8:8080/" // Substitute IP to access localhost
+    private const val BASE_URL = "https://projeto-pdm-artesao-api-production.up.railway.app/"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
